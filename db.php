7c3ffda5e3caf5fdabeb6fdabb22b2523cf792a1
raicomputer <134986060+raicomputer@users.noreply.github.com>
2026-03-03 14:14:13 +0530

    Add files via upload

--- db.php
+++ db.php
@@ -1,19 +1,14 @@
 <?php
+
 $host = "sql12.freesqldatabase.com";
 $user = "sql12818488";
 $password = "AiNkWRH4Q5";
 $database = "sql12818488";
 
-$conn = mysqli_connect($host, $user, $password, $database);
-
-if (!$conn) {
-    die("❌ Connection Failed: " . mysqli_connect_error());
-}
-
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
-?>
 
+?>
